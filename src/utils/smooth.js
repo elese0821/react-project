a9d32c92f5073b11ca762757c269a1f8ac34707e
elese0821 <elese0821>
2023-11-09 12:11:19 +0900

    Update smooth.js

--- src/utils/smooth.js
+++ src/utils/smooth.js
@@ -3,7 +3,8 @@ import Lenis from '@studio-freight/lenis'
 export function smooth() {
     const lenis = new Lenis({
         duration: 1,
-        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        smooth: true
     })
 
     lenis.on('scroll', (e) => {
